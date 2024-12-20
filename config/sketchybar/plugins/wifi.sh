300e0f53c7cdedf3fcba437e63af05723bc13fcb
1r0sh <72923299+1r0sh@users.noreply.github.com>
2024-12-20 12:13:23 +0330

    delete old files

--- config/sketchybar/plugins/wifi.sh
+++
@@ -1,9 +0,0 @@
-source "$HOME/.config/colors.sh"
-source "$HOME/.config/icons.sh"
-
-HOST_NAME=$(ipconfig getsummary en0 | awk -F ' SSID : ' '/ SSID : / {print $2}')
-
-sketchybar --set $NAME \
-  icon=$ICON_WIFI \
-  label="$HOST_NAME" \
-  click_script="osascript -e 'tell application \"System Events\" to tell process \"Control Center\" to perform action \"AXPress\" of menu bar item 3 of menu bar 1'"
