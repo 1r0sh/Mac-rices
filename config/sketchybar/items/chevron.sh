035bd1be6046f1aeb0d8cdfb0f501ba254bae015
dex <arash@OS.local>
2025-01-14 19:06:58 +0330

    delete some files

--- config/sketchybar/items/chevron.sh
+++
@@ -1,2 +0,0 @@
-sketchybar --add item chevron left \
-	--set chevron icon="􀰩" label.drawing=off icon.font="CaskaydiaCove Nerd Font:Regular:16.0"
--- config/sketchybar/items/front_app.sh
+++
@@ -1,4 +0,0 @@
-sketchybar --add item front_app left \
-	--set front_app script="$PLUGIN_DIR/front_app.sh" \
-	label.font="CaskaydiaCove Nerd Font:Regular:14.0" \
-	--subscribe front_app front_app_switched
